a9c6ab588fe93339d12aac8044b23360301991d8
Brendan Ly <brendanly4@gmail.com>
2024-10-19 19:29:24 -0700

    added back submit button

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,49 +1,57 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import axios from 'axios';
-import logo from '/src/img/logo-v2-transparent.png';
+import axios from "axios";
+import logo from "/src/img/logo-v2-transparent.png";
 
 export default function Quiz() {
   const [questions, setQuestions] = useState([]);
   const [inputText, setInputText] = useState("");
   const [response, setResponse] = useState("");
 
-// boilerplate flask
+  // boilerplate flask
   const handleSubmit = async (e) => {
-    e.preventDefault();  // Prevent page reload on form submit
-    try { // Set response from Flask API
-      const res = await axios.post('http://localhost:5000/send-text',  { inputText })
-    } 
-    catch (error) {
+    e.preventDefault(); // Prevent page reload on form submit
+    try {
+      // Set response from Flask API
+      const res = await axios.post("http://localhost:5000/send-text", {
+        inputText,
+      });
+    } catch (error) {
       console.error("There was an error!", error);
     }
   };
-  const INPUT_CLASS = 'w-full max-w-2xl indent-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-newpurple-500 sm:text-sm sm:leading-6';
-
+  const INPUT_CLASS =
+    "w-full max-w-2xl indent-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-newpurple-500 sm:text-sm sm:leading-6";
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   return (
     <div className="container mx-auto mt-8">
       <div className="flex">
         <div className="w-1/4 bg-newpurple-500 p-8 rounded-none shadow-md">
-          <h4 className="text-xl font-semibold text-white mb-4"><img src={logo} alt="Logo" width={50}/>Menu</h4>
+          <h4 className="text-xl font-semibold text-white mb-4">
+            <img src={logo} alt="Logo" width={50} />
+            Menu
+          </h4>
           <ul className="space-y-2">
-          <li>
-              <button onClick = {handleButtonClick} className="w-full text-left p-2 text-white hover:bg-newpurple-700">
+            <li>
+              <button
+                onClick={handleButtonClick}
+                className="w-full text-left p-2 text-white hover:bg-newpurple-700"
+              >
                 Home
               </button>
             </li>
-            
+
             <li>
               <button className="w-full text-left p-2 text-white hover:bg-newpurple-700">
                 Quiz 1
@@ -66,7 +74,10 @@ export default function Quiz() {
           <h1 className="text-2xl font-bold text-center mt-4 font-mono">
             What can I quiz you on today?
           </h1>
-          <div className="flex justify-center mt-4">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center mt-4 space-y-4"
+          >
             <input
               type="text"
               placeholder="Enter your notes, or a subject!"
@@ -74,8 +85,13 @@ export default function Quiz() {
               value={inputText}
               onChange={handleInputChange}
             />
-          </div>
-
+            <button
+              className="bg-newpurple-500 hover:bg-newpurple-700 text-white px-4 py-2 rounded-md"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
           <div className="flex justify-center mt-4">
             <input // set to output later
               type="text"
